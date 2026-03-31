a795965ba39e2a9ea3bce32ada5a7a6e0c5c5a37
khaeru99 <khaerul.r.23@gmail.com>
2026-04-01 01:24:38 +0700

    update

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,7 +14,8 @@ app.use('/tasks', taskRoutes);
 // Health Check
 app.get('/', (req, res) => res.send('API To-Do List Running 🚀'));
 
-const PORT = 3000;
-app.listen(PORT, () => {
+const PORT = Number(process.env.PORT) || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Server berjalan di http://localhost:${PORT}`);
 });
